963220855e8ae8824b7b0be2b4b890b1a529bab2
paulccampos <paulccampos@gmail.com>
2025-04-09 18:12:57 +0800

    App.java

--- exercise2/EXERCISE 2/src/App.java
+++ exercise2/EXERCISE 2/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Car myCar = new Car("10kWh", 6);
+        System.out.println("Battery: " + myCar.getBattery());
+        System.out.println("Cylinder: " + myCar.getCylinder());
     }
 }
